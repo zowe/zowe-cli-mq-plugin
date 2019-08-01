655f665c7565a3d5ddfba4b4965ee543c198df9d
zFernand0 <fernando.rijocedeno@broadcom.com>
2019-08-01 08:54:02 -0400

    Change name for conformant plugin - Naming changed onf various md files - Package name changed - Coverage results set to be the same or higher than `base`
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

--- __tests__/__src__/environment/TestEnvironment.ts
+++ __tests__/__src__/environment/TestEnvironment.ts
@@ -173,7 +173,7 @@ export class TestEnvironment {
     private static async installPlugin(testEnvironment: ITestEnvironment) {
         let installScript: string = TemporaryScripts.SHEBANG;
         installScript += "zowe plugins install ../../../../\n"; // install plugin from root of project
-        installScript += "zowe plugins validate @zowe/mq";
+        installScript += "zowe plugins validate @zowe/mq-for-zowe-cli";
         installScript += "zowe mq --help\n"; // check that the plugin help is available
         const scriptPath = testEnvironment.workingDir + "/install_plugin.sh";
         IO.writeFile(scriptPath, Buffer.from(installScript));

7dd7abae2eef01c07f4ec6619ead130bfab61336
zFernand0 <fernando.rijocedeno@broadcom.com>
2022-01-20 15:29:33 +0000

    update test
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

--- __tests__/__integration__/cli/cli.mqprofile.integration.test.ts
+++ __tests__/__integration__/cli/cli.mqprofile.integration.test.ts
@@ -31,8 +31,8 @@ describe("Creating an MQ profile", () => {
 
     it("should create an MQ profile successfully with fake connection details", () => {
         const output = runCliScript(__dirname + "/__scripts__/create_mq_profile.sh", testEnvironment);
-        expect(TempTestProfiles.isStderrEmpty(output.stderr)).toBeTruthy();
-        expect(output.status).toEqual(0);
+        expect(output.stderr.toString()).toContain("The command 'profiles create' is deprecated.");
         expect(output.stdout.toString()).toContain("success");
+        expect(output.status).toEqual(0);
     });
 });
